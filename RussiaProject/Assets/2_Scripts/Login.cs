92cfc53a6f42398010d066cefa930316b6c0b090
한승훈 <whcktnr@naver.com>
2021-03-15 02:08:51 +0900

    DoozyUI 업데이트

---
+++ RussiaProject/Assets/2_Scripts/Login.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+
+    public string userId;
+    public string userPassword;
+
+}
